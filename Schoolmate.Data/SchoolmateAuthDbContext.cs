53b5d9916ac7d1ab91c2d98ee1b24ea4e05c303e
ej <ejgeneral27@gmail.com>
2020-03-15 20:48:23 -0500

    successfully created initial migration

---
+++ Schoolmate.Data/SchoolmateAuthDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Schoolmate.Data
+{
+    public class SchoolmateAuthDbContext : IdentityDbContext<IdentityUser>
+    {
+        public SchoolmateAuthDbContext(DbContextOptions<SchoolmateAuthDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Schoolmate.Data/TemporaryDbContextFactory.cs
@@ -0,0 +1,44 @@
+﻿using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Schoolmate.Data
+{
+    public class TemporarySchoolmateAuthDbContextFactory : IDesignTimeDbContextFactory<SchoolmateAuthDbContext>
+    { 
+        public SchoolmateAuthDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<SchoolmateAuthDbContext>();
+            optionsBuilder.UseSqlServer("Server=localhost;Database=schlmate_auth;User Id=sa;Password=$$GiGatT777;");
+
+            return new SchoolmateAuthDbContext(optionsBuilder.Options);
+        }
+    }
+
+    public class TemporaryPersistedGrantDbContextFactory : IDesignTimeDbContextFactory<PersistedGrantDbContext>
+    {
+        public PersistedGrantDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<PersistedGrantDbContext>();
+            optionsBuilder
+                .UseSqlServer("Server=localhost;Database=schlmate_auth;User Id=sa;Password=$$GiGatT777;",
+                    builderOptions => builderOptions.MigrationsAssembly("Schoolmate.Data"));
+
+            return new PersistedGrantDbContext(optionsBuilder.Options, new OperationalStoreOptions());
+        }
+    }
+
+    public class TemporaryConfigurationDbContextFactory : IDesignTimeDbContextFactory<ConfigurationDbContext>
+    {
+        public ConfigurationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ConfigurationDbContext>();
+            optionsBuilder
+                .UseSqlServer("Server=localhost;Database=schlmate_auth;User Id=sa;Password=$$GiGatT777;",
+                    builderOptions => builderOptions.MigrationsAssembly("Schoolmate.Data"));
+
+            return new ConfigurationDbContext(optionsBuilder.Options, new ConfigurationStoreOptions());
+        }
+    }
+}
